e129384e1c35524bac02ce44f9ca7865773e1235
Phillip <75212736+deepelegant99@users.noreply.github.com>
2025-12-16 13:01:32 -0800

    Document permutation CLI usage

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hello from codespace")
+from permutation import word_permutations
+
+if __name__ == "__main__":
+    hello_permutations = word_permutations("Hello")
+    print(hello_permutations)
